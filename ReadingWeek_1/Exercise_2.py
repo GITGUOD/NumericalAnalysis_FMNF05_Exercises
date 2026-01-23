ce48cae4fc945eee1aba27d758220ef7696af750
GITGUOD <151746872+GITGUOD@users.noreply.github.com>
2026-01-23 20:24:04 +0100

    FloatToAnyBase Added

--- ReadingWeek_1/Exercise_2.py
+++ ReadingWeek_1/Exercise_2.py
@@ -9,3 +9,24 @@ print(Evaluate(0.4, 0.3))
 
 # Output: 0.10000000000000003
 
+def makeFloatToAnyBase(number, base, precision=17):
+    newNumberConversion = "0."
+    frac = number
+    digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    for _ in range(precision):
+        frac *= base
+        digit = int(frac)
+        newNumberConversion += digits[digit]
+        frac -= digit
+
+        if frac == 0:
+            break
+
+    return newNumberConversion
+
+a = makeFloatToAnyBase(0.4, 2)
+b = makeFloatToAnyBase(0.3, 2)
+print(a, b)
+
+print(0.01100110011001100 - 0.01001100110011001)
