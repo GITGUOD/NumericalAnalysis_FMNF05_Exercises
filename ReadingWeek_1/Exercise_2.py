87d5ec6ada9cf3dd742495767e49f7414431c9ed
GITGUOD <151746872+GITGUOD@users.noreply.github.com>
2026-01-23 19:41:37 +0100

    First problem solved

---
+++ ReadingWeek_1/Exercise_2.py
@@ -0,0 +1,9 @@
+# Problem: Evaluate 0.4 - 0.3 with Python or Matlab with up to 17 significant digits
+
+# First partproblem: What do you observe? Explain what happened.
+
+def Evaluate(a, b):
+    return a-b
+
+print(Evaluate(0.4, 0.3))
+
